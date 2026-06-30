630e9186f91a94005f9659fa3bddad0fd75163f1
Riya Sharma <rsharma56978@gmail.com>
2026-06-30 20:10:46 +0530

    Complete Rock Paper Scissors game

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -1,1 +1,15 @@
 import random
+CHOICES = ['rock', 'paper','scissors']
+def get_computer_choices():
+    choice =random.choice(CHOICES)
+    
+def decide_winner(player , computer):
+    if player == computer:
+        return "draw"
+    winning_moves ={
+        "rock" : "scissors",
+        "paper": "rock",
+        "scissors":"paper"
+        
+    }
+        
