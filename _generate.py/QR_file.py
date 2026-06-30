190a2a23a00e3667b73885391f73255cad762296
Riya Sharma <Piyush.tech2024@gmail.com>
2026-06-30 22:41:52 +0530

    some changes occurs to make it attractive

--- _generate.py/QR_file.py
+++ _generate.py/QR_file.py
@@ -1,6 +1,11 @@
 import qrcode
+from PIL import Image
+# img = qrcode.make("https://github.com/PiyushAggarwal01")
+# img.save("piyush_github.png")
 
-img = qrcode.make("https://github.com/PiyushAggarwal01")
-img.save("piyush_github.png")
-
-print("QR Code Created Successfully!")
+# print("QR Code Created Successfully!")
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border = 4,)
+qr.add_data("https://www.linkedin.com/in/piyush-aggarwal-b51544324/")
+qr.make(fit=True)
+img = qr.make_image(fill_color = "red", back_color = "blue")
+img.save("piyush_linkin.png")
