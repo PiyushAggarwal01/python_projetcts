4e0c8808742dc76707567e6dffe1d9d99b83e121
Riya Sharma <Piyush.tech2024@gmail.com>
2026-06-30 21:01:49 +0530

    Complete Rock Paper Scissors game

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -12,4 +12,40 @@ def decide_winner(player , computer):
         "scissors":"paper"
         
     }
+    if winning_moves[player] == computer:
+        return "draw"
+    
+    return computer
+
+def playgame():
+    print(" Enter to the rock , paper  , scissors game .")
+    
+    while True:
+        player = input(" please select one from (rock , paper , scissors):").strip().lower()
         
+        if player not in CHOICES:
+            return " invalid syntax please enter valid choices"
+        
+        computer = get_computer_choices
+        result  = decide_winner(player , computer)
+           
+        
+        print(f"your choices:{player}") 
+        print(f"computer choice:{computer}")   
+        
+        if result == player:
+            print("you win the game")
+            
+        if result == computer:
+            print(" computer win ") 
+            
+        if result == draw :
+            print(" match tied")
+            
+        play_again = input(" DO YOU WANT TO PLAY AGIAN : YES /NO").strip().lower()
+            
+        if play_again != 'yes':
+            print(" THANKS FOR PLAYING")
+            break
+        
+playgame()
